c0601adffada16371b5ddf20360af48132cc1850
PRAKHAR PANDEY <prakhar.computersciencebusiness@gmail.com>
2024-01-07 23:01:19 +0530

    NodeJs, User Service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { UserCreationDTO } from '../interfaces/user-creation.dto'; // Import the UserCreationDTO interface
 import { hashPassword } from '../utils/password.utils';
+import { User } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class UserService {
+  async login(credentials: { username: string; password: string; }) {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly userRepository: UserRepository) {}
 
   async signup(userData: UserCreationDTO): Promise<User | null> {
